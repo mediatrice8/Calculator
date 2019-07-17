36c481385a120002ce8260136c0b04e1358491d1
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-17 12:45:44 +0200

    i change the operation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var add = function(number1,number2){
+var substraction= function(number1,number2){
     return number1 + number2;
     };
     var number1 = parseInt(prompt( "enter the number"));
     var number2 = parseInt(prompt("enter another number"));
-    var result = add(number1 ,number2);
+    var result = substraction(number1 ,number2);
     alert (result);
