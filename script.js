b9ced4e5f5eac0f6343317e0eb3e0bb611d857e7
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-17 13:01:09 +0200

    i perfom division

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ var add= function(number1,number2){
     var multiply =function(number1,number2){
         return number1 * number2;
     };
+    var divide = function(number1,number2){
+        return number1 /number2;
+    };
     var number1 = parseInt(prompt( "enter the number"));
     var number2 = parseInt(prompt("enter another number"));
-    var result = multiply(number1 ,number2);
+    var result = divide(number1 ,number2);
     alert (result);
