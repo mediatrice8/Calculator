f777dc581b9e3498d972bb5836eeef3cf08b3207
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-17 12:58:16 +0200

    i perfom multiply

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ var add= function(number1,number2){
     var subtract =function(number1,number2){
         return number1 -number2;
     };
+    var multiply =function(number1,number2){
+        return number1 * number2;
+    };
     var number1 = parseInt(prompt( "enter the number"));
     var number2 = parseInt(prompt("enter another number"));
-    var result = subtract(number1 ,number2);
+    var result = multiply(number1 ,number2);
     alert (result);
