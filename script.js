3958ada3feef4368c6ca88dbc6df0e2979d0d5a8
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-17 12:53:30 +0200

    i perfom substract

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var substraction= function(number1,number2){
+var add= function(number1,number2){
     return number1 + number2;
+    };
+    var subtract =function(number1,number2){
+        return number1 -number2;
     };
     var number1 = parseInt(prompt( "enter the number"));
     var number2 = parseInt(prompt("enter another number"));
-    var result = substraction(number1 ,number2);
+    var result = subtract(number1 ,number2);
     alert (result);
