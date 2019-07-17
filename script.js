02bbaf9afbf8fb632f4dcf83c0ac040a4b54e26d
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-17 12:26:09 +0200

    i add change to the contents

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ var add = function(number1,number2){
     };
     var number1 = parseInt(prompt( "enter the number"));
     var number2 = parseInt(prompt("enter another number"));
-    alert (add(number1,number2));
+    var result = add(number1 ,number2);
+    alert (result);
